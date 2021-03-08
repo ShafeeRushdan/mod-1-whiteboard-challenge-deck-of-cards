d04af5e95e7b0e9a768ea3caf9c3c394ed6f3db0
github username <github email address>
2021-03-08 09:44:14 -0500

    commit tree 7459ac513de208c1389f1efa855a133357656cd5

--- solution.rb
+++ solution.rb
@@ -1,7 +1,36 @@
 class Deck
+    attr_reader :card
+    @@all =[]
+    def initialize :card
+        @cards = card
+
+        def self.all
+            @@all
+        end
+    end
+
+    
+        
 
 end
 
 class Card
+    rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    
+    attr_reader :rank, :suit
+    @@all = []
+    
+    def initialize :suit, :rank
+        @suit = suit
+        @rank = rank
+
+    end
+
+
+
+    def self.all
+        @@all
+    end
 
 end
